2e66d3f11ff8649951717883c517732dcc5497eb
akshat16206 <akshat8036@gmail.com>
2025-03-27 02:36:00 +0530

    my first contract using basic function (to store the favourite value

--- contracts/my-first-contract.sol
+++ contracts/my-first-contract.sol
@@ -3,4 +3,16 @@ pragma solidity ^0.8.29; //version
 
 contract MyFirstContract { 
     //code which is contract
-}
+    // boolean , int ,unit, address, byte
+    bool FavouriteNumber = false;
+    int number = 23;
+    uint8  Myunit = 123 ;
+    address abc = 0x35Eb25216E74f267B6Bf4A7b927A95FC674BB0ca;
+    string favnnu = "five";
+    uint xyz; //this gets default value as 0
+
+    uint256 public favouritenumber ; 
+    function store(uint _favouritenumber) public {
+   favouritenumber = _favouritenumber;
+} 
+}//0x7EF2e0048f5bAeDe046f6BF797943daF4ED8CB47

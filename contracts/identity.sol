cb1e4587f0eb42aaad00af304f1b7f34fff9caf1
akshat16206 <akshat8036@gmail.com>
2025-03-28 16:40:46 +0530

    identity.sol added

---
+++ contracts/identity.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.29; //version
+
+contract identity {
+   string name;
+   uint age;
+
+   constructor() public 
+   {
+    name = "akshat";
+    age = 19;
+   }
+   function getName() public view returns(string memory) {
+    return name;
+   }
+   function getAge() public view returns(uint) {
+    return age;
+   }
+   function setAge() public {
+      age=age+1;
+   }
+}

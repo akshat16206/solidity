f5e00e826e2044303a662095dccf72e06e1b99aa
akshat16206 <akshat8036@gmail.com>
2025-03-29 03:04:10 +0530

    byte array

---
+++ contracts/bytearray.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.29;
+
+contract array{
+    bytes2 public b2; //2 byte array
+    bytes3 public b3; //3 byte array
+ function setter() public {
+    b2="Ab";
+    b3 = "abc";
+ }
+}

9e4b8bad1f246ab3cb0ea923daa3c463d3064509
akshat16206 <akshat8036@gmail.com>
2025-03-28 16:39:30 +0530

    constructor.sol added

---
+++ contracts/constructor.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.29;
+
+contract con{
+    uint public count;
+    constructor(uint newcount)  {
+        count = 4;
+        count = newcount;
+    }
+}

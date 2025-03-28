0107c15160ee6897aa9c2f23186587c695b6c6d5
akshat16206 <akshat8036@gmail.com>
2025-03-29 01:47:47 +0530

    array in soloidity

---
+++ contracts/array.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MITS
+pragma solidity ^0.8.19;
+//fixed size array
+contract array{
+    uint[4] public arr = [10,20,30,40];
+    function setter(uint index,uint value) public  {
+        arr[index]=value;
+    }
+}

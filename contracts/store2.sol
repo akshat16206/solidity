5cc3345bce48a5ecae3a23d06af188bd1c1b42fd
akshat16206 <akshat8036@gmail.com>
2025-03-28 16:30:21 +0530

    update

--- contracts/store2.sol
+++ contracts/store2.sol
@@ -12,6 +12,7 @@ contract storefavnumber {
     People public person2 = People({favouritenumber: 8,name: "patrick"});
     People public person3 = People({favouritenumber: 45,name: "ally"});
     People public person4 = People({favouritenumber: 9,name: "bob"});
+//this is not ab better way t0 use struct. a better way is to use array. see store3.sol
 
 
     function store(uint256 _favouritenumber) public {

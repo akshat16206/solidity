5c8c28aeda1436cc431d4349cadd5bfc692a4653
akshat16206 <akshat8036@gmail.com>
2025-04-14 16:51:04 +0530

    update

--- contracts/bytearray2.sol
+++ contracts/bytearray2.sol
@@ -2,5 +2,9 @@
 pragma solidity 0.8.29;
 
 contract array{
+    //1 byte = 8bits
+    // 1 hexadecimal digit = 4 bits
+    //everything hat will be stored in the byte array ill be in the form of hexadecimal digits.
     
+
 }
